6ce2870bac8777b1d6296ba7cb51794fe3cc1094
onticinc <onticinc@gmail.com>
2021-12-01 14:40:20 -0700

    created userInfo model

---
+++ models/userinfo.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class userInfo extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  userInfo.init({
+    userName: DataTypes.STRING,
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.INTEGER,
+    email: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'userInfo',
+  });
+  return userInfo;
+};
